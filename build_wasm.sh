31eed2d830b55e3baf8679bb7c75ff2695678bb5
Ian Platt <ianplatt@Ians-MacBook-Pro.local>
2024-02-16 19:58:45 -0800

    Build index.html for wasm

--- build_wasm.sh
+++ build_wasm.sh
@@ -13,3 +13,29 @@ wasm-bindgen --no-typescript --target web \
     --out-dir ./out/ \
    --out-name "$PROJECT_NAME" \
    ./target/wasm32-unknown-unknown/release/$PROJECT_NAME.wasm
+
+cat > ./out/index.html <<- EOM
+<!doctype html>
+<html lang="en">
+    <body style="margin: 0px">
+        <script type="module">
+            import init from "./combative-survivors.js";
+
+            init().catch((error) => {
+                if (
+                    !error.message.startsWith(
+                        "Using exceptions for control flow, don't mind me. This isn't actually an error!",
+                    )
+                ) {
+                    throw error;
+                }
+            });
+        </script>
+        <canvas
+            id="combative-survivors-canvas"
+            width="1280"
+            height="720"
+        ></canvas>
+    </body>
+</html>
+EOM
